034d2f0752e2f3a0f67d58325fad5cc512ecd10e
Igor Ferreira <48239402+imagno@users.noreply.github.com>
2022-03-22 17:30:34 -0300

    implementado o viewset

--- api/views.py
+++ api/views.py
@@ -1,3 +1,9 @@
+from api.serializers import UsuariosSerializer
+from rest_framework import viewsets, permissions
+from api.models import Usuarios
 from django.shortcuts import render
 
-# Create your views here.
+class UsuariosViewSet(viewsets.ModelViewSet):
+  queryset = Usuarios.objects.all()
+  serializer_class = UsuariosSerializer
+  permission_class = [permissions.IsAuthenticated]
