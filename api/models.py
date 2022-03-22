ced840c4085347bd5f66c73285b66ca14ce20f16
Igor Ferreira <48239402+imagno@users.noreply.github.com>
2022-03-22 17:13:10 -0300

    gerada a model e migrada para o db

--- api/models.py
+++ api/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+import uuid
+from django.core.management.utils import get_random_secret_key
 
-# Create your models here.
+class Usuarios(models.Model):
+  
+  id = models.UUIDField(
+    primary_key=True,
+    default=uuid.uuid4,
+    null=False,
+    blank=True,
+  )
+  login = models.CharField(
+    max_length=8,
+    null=False,
+    blank=False,
+  )
+  senha = models.CharField(
+    max_length=50,
+    null=False,
+    blank=True,
+    default=get_random_secret_key,
+  )
+  data_nascimento = models.DateField(
+    null=False,
+    blank=False,
+  )
